042a8e8e493658d15d0fcecc237d6b80cfb43554
anatoly <glenglen174@yandex.ru>
2025-04-24 16:24:23 +0500

    add

--- firstScript.js
+++ firstScript.js
@@ -2,5 +2,9 @@ let firstLet = 1;
 const dirstConst 5;
 firstLet = 10;
 console.log(firstLet);
+//Logging first variables
 console.log("firstLet", firstLet);
 console.log("const", firstLet);
+/*
+	Comments
+*/
